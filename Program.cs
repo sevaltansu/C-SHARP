79e27412b82c3a3f654ae9f8429189fc7148ec50
sevaltansu <72619752+sevaltansu@users.noreply.github.com>
2020-10-12 18:39:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHİLE_ÖRNEK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 1;
+            int adet = 0;
+            int sayi = 0;
+            int toplam = 0;
+            int ort = 0;
+            Console.WriteLine("kaç adet sayı girmek istediğinizi girin : ");
+            adet = Convert.ToInt32(Console.ReadLine());
+            while (sayac<=adet)
+            {
+                Console.WriteLine("{0}. sayıyı giriniz",sayac);
+                sayi = Convert.ToInt32(Console.ReadLine());
+                toplam = toplam + sayi;
+                sayac++;
+
+            } ort = toplam / adet;
+            Console.WriteLine("girdiğiniz sayıların ortalaması :{0}", ort);
+
+             Console.ReadLine();
+        }
+    }
+}
