aef14197dc94250a2673685c43e3d4d0cd461592
sevaltansu <72619752+sevaltansu@users.noreply.github.com>
2020-10-13 15:53:22 +0300

    Add files via upload
    DOWHİLE

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOWHİLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kad = "seval", ksifre = "123";
+            string kullacıcıad, kullanıcısifre;
+            int hak = 3;
+            do
+            {
+                Console.WriteLine("kullanıcı adını giriniz : ");
+                kullacıcıad = Console.ReadLine();
+                Console.WriteLine("kullanıcı şifresini giriniz : ");
+                kullanıcısifre = Console.ReadLine();
+                if (kullacıcıad==kad && kullanıcısifre==ksifre)
+                {
+                    Console.WriteLine("BAŞARIYLA GİRİŞ YAPTINIZ!");
+
+
+                }
+                else
+                {
+                    hak--;
+                    if (hak==0)
+                    {
+                        Console.WriteLine("GİRİŞ HAKKINIZI KAYBETTİNİZ. MÜŞTERİ HİZMETLERİNE BAĞLANIN ");
+                    }
+                }
+
+            } while ((kullanıcısifre!=ksifre || kullacıcıad!=kad)&& hak!=0);
+            Console.ReadKey();
+            
+        }
+    } 
+}
