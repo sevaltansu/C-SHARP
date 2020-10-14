63bd1de2a31e5187721175011d6e423058df4c8c
sevaltansu <72619752+sevaltansu@users.noreply.github.com>
2020-10-14 23:00:39 +0300

    Add files via upload

---
+++ timer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TİMER
+{
+    class Program
+    {
+        private static int x =10;
+            private static Timer sayac = new Timer();
+        static void Main(string[] args)
+        {
+            sayac.Interval = 1000;
+            sayac.Elapsed += görev;
+            sayac.Start();
+            Console.ReadKey();
+          
+            }
+        private static void görev(object source, System.Timers.ElapsedEventArgs e)
+        {
+            x--;
+            Console.Clear();
+            Console.WriteLine(x);
+            if (x==0)
+            {
+                Console.WriteLine("süre doldu!!");
+                sayac.Stop();
+
+            }
+            
+        }
+        }          
+     
+    }
+
